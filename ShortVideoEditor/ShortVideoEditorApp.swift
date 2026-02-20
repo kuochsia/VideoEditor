2666cfd1ab5ebcdbcfd648422f2d6dff2ab43b3e
Siav <kuochsia@gmail.com>
2026-02-20 16:55:09 +0100

    Gemini first version

---
+++ ShortVideoEditor/ShortVideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoEditorApp.swift
+//  ShortVideoEditor
+//
+//  Created by siav-kuong kuoch on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShortVideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
